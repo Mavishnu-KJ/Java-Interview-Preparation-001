10e53ba87ea9d1762de8fbc9418b9a855785e326
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-06-20 01:51:57 +0530

    Java 8-17 Coding practices

--- src/main/java/exercises0007/MainClassForExercises0007.java
+++ src/main/java/exercises0007/MainClassForExercises0007.java
@@ -2,8 +2,10 @@ package exercises0007;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MainClassForExercises0007 {
 
@@ -14,20 +16,33 @@ public class MainClassForExercises0007 {
         //Java, Spring Boot, Hibernate
         // arrayformat, Hibernate, Spring Boot, Java
         List<String> stringList = Arrays.asList("Java", "Spring Boot", "Hibernate");
-
-        String[] stringArray = new String[stringList.size()];
+        String[] outputStringArray = new String[stringList.size()];
         int k = stringList.size();
 
         for(int i = stringList.size() -1, j=0; i>=0 && j<k; i--, j++){
 
             if(j<k) {
-                stringArray[j] = stringList.get(i);
+                outputStringArray[j] = stringList.get(i);
 
             }
 
         }
         System.out.println("String List : "+stringList);
-        System.out.println("Reverse order : "+Arrays.toString(stringArray));
+        System.out.println("Reverse order : "+Arrays.toString(outputStringArray));
+
+        String[] outputStringArray1 = stringList.stream()
+                .collect(
+                        Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        list -> {
+                            Collections.reverse(list);   // Reverse the list
+                            return list.toArray(new String[0]);
+                        }
+                ));
+
+        System.out.println("Original List     : " + stringList);
+        System.out.println("Reversed Array    : " + Arrays.toString(outputStringArray1));
+
 
 
 
