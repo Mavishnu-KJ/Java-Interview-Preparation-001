f585a408f403e87a1ff4cec9a8693e7ed32234c0
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-06-20 02:31:48 +0530

    Java 8-17 Coding practices

--- src/main/java/exercises0008/MainClassForExercises0008.java
+++ src/main/java/exercises0008/MainClassForExercises0008.java
@@ -1,9 +1,8 @@
 package exercises0008;
 
-import exercises0008.MainClassForExercises0008;
-
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MainClassForExercises0008 {
 
@@ -12,6 +11,25 @@ public class MainClassForExercises0008 {
             String name,
             String department,
             Double salary
+    ){
+
+    }
+
+    public static record Item(
+            String itemName
+    ){
+
+    }
+
+    public static record Order(
+            List<Item> itemList
+    ){
+
+    }
+
+    public static record Transaction(
+            String transactionId,
+            Double amount
     ){
 
     }
@@ -33,74 +51,18 @@ public class MainClassForExercises0008 {
                 new Employee(7, "Jadeja", "All Rounder", 444444.0)
         );
 
-        Map<String, Employee> highestPaidEmployeeMapByDepartmentWise = employeeList.stream()
-                .collect(Collectors.groupingBy(
-                        Employee::department,
-                        Collectors.maxBy(Comparator.comparingDouble(Employee :: salary))
-                )).entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry :: getKey,
-                        entry -> entry.getValue().orElse(null)
-                ));
-
-        System.out.println("=".repeat(20));
-        System.out.println("Highest paid employee department wise : ");
-        highestPaidEmployeeMapByDepartmentWise.forEach((dept, emp) -> {
-           System.out.println(dept+"-> "+emp.name+"( Salary : "+ emp.salary()+" )");
-        });
-
-        Map<String, Employee> secondHighestPaidEmployeeMapByDepartmentWise = employeeList.stream()
-                .collect(Collectors.groupingBy(
-                        Employee :: department,
-                        Collectors.collectingAndThen(
-                                Collectors.toList(),
-                                list -> {
-                                    if(list.size() < 2) return null;
-
-                                    list.sort((e1, e2) -> Double.compare(e2.salary(), e1.salary()));
-                                    return list.get(1);
-                                }
-                        )
-                )).entrySet().stream()
-                .filter(entry -> entry.getValue() != null)
-                .collect(Collectors.toMap(
-                        Map.Entry :: getKey,
-                        entry -> entry.getValue()
-                ));
-
-        System.out.println("=".repeat(20));
-        System.out.println("Second highest paid employee department wise : ");
-        secondHighestPaidEmployeeMapByDepartmentWise.forEach((dept, emp) -> {
-                System.out.println(dept + "-> " + emp.name + "( Salary : " + emp.salary() + " )");
-        });
+        //Highest paid employee
 
 
-         /*
+        //Second highest paid employee
+
+
+
+        /*
         Frequency Count: Given a string,
         find the frequency of each character, and filter out the top 3 most frequent elements using Streams.
         */
-
-        String str = "aaabbbbbbcccdddd";
-        Map<Character, Integer> top3MostFrequentElements = str.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(
-                        c -> c,
-                        Collectors.counting()
-                )).entrySet().stream()
-                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
-                .limit(3)
-                .collect(Collectors.toMap(
-                        Map.Entry :: getKey,
-                        entry -> entry.getValue().intValue(),
-                        (existing, duplicate) -> existing,
-                        LinkedHashMap :: new
-                ));
-
-        System.out.println("=".repeat(20));
-        System.out.println("top 3 most frequent elements using Streams : ");
-        top3MostFrequentElements.forEach((character, frequency) -> {
-            System.out.println(character + "-> "+frequency+" times repeated");
-        });
+        String str1 = "aaabbbbbbcccdddd";
 
         /*
         Frequency Count: Given a list of words,
@@ -112,25 +74,100 @@ public class MainClassForExercises0008 {
                 "banana", "orange", "banana", "apple", "orange"
         );
 
-        Map<String, Integer> top3MostFrequentElements1 = wordList.stream()
-                .collect(Collectors.groupingBy(
-                        s -> s,
-                        Collectors.counting()
-                )).entrySet().stream()
-                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
-                .limit(3)
-                .collect(Collectors.toMap(
-                        Map.Entry :: getKey,
-                        entry -> entry.getValue().intValue(),
-                        (existing, duplicate) -> existing,
-                        LinkedHashMap :: new
-                ));
+        /*
+        Flattening Nested Collections: Given a list of Order objects, where each order contains a list of Item objects,
+        extract a distinct, sorted list of all item names across all orders using flatMap.
+        */
 
-        System.out.println("=".repeat(20));
-        System.out.println("top 3 most frequent elements using Streams : ");
-        top3MostFrequentElements1.forEach((word, freq) -> {
-            System.out.println(word + "-> "+freq+" times repeated");
-        });
+        List<Order> orderList = List.of(
+                new Order(List.of(new Item("Keyboard"), new Item("Mouse"))),
+                new Order(List.of(new Item("Laptop"), new Item("Bag"))),
+                new Order(List.of(new Item("Keyboard"), new Item("Mobile"))),
+                new Order(List.of(new Item("Tab"), new Item("Mouse"))),
+                new Order(List.of(new Item("Pen")))
+        );
+
+
+
+        /*
+        Partitioning Data: Given a list of transactions,
+        partition them into two groups (e.g., transactions above ₹50,000 and below) and
+        calculate the average transaction amount for each group simultaneously.
+        */
+
+        List<Transaction> transactionList = List.of(
+                new Transaction("10", 80000.0),
+                new Transaction("18", 60000.0),
+                new Transaction("7", 70000.0),
+                new Transaction("3", 40000.0),
+                new Transaction("1", 45000.0),
+                new Transaction("15", 20000.0)
+        );
+
+
+        /*
+        Custom String Joining: Given a list of strings, filter out empty strings,
+        convert them to uppercase, and join them with a comma separator enclosed in square brackets [A, B, C].
+        */
+
+        List<String> stringList = Arrays.asList(
+                "Sachin", "Shewag", "Gambhir", "Virat", "", "Yuvraj", null, "Dhoni", "   ", "Raina"
+        );
+
+        /*
+        Data Aggregation: Given a collection of complex Employee,
+        use Streams to sort them dynamically by department name first, and then by salary descending.
+        */
+
+
+        /*
+        Find the number which is repeated the same number of times
+        Given List : [12,13,2,2,3,5,4,4,4,4], Can you guess the output?
+        Output {2,4}
+        */
+
+        List<Integer> integerList = List.of(12,13,2,2,3,5,4,4,4,4);
+
+
+        /*
+        Return the consecutive two numbers 2 peer. Output will be List<List>
+        Given list [1,2,4,5,7,8,9], Output {{1,2}, {4,5}, {7,8}, {8,9}}
+        */
+
+        List<Integer> integerList1 = List.of(1,2,4,5,7,8,9);
+
+
+        /*
+        Print non-repeating first character. Consider any String. your wish.
+        */
+
+        String s1 = "aabbcddxxyyza";
+
+        /*
+        Binary Grouping: Take a number in binary form and count the groups of isolated 0s individually before a 1 appears
+        (e.g., Input: 0011000 → Output: groups of size 2 and 3).
+        */
+
+        /*
+        Time Complexity Explanation:
+        O(n) — String split and stream operations process each character linearly.
+        Space Complexity Explanation:
+        O(n) — For storing the resulting list of group sizes.
+        */
+
+        String binaryNumber = "0011000";
+
+
+        /*
+        Merge Overlapping Intervals: Given a collection of intervals (e.g., [1,3], [2,6], [8,10]),
+        merge all overlapping intervals to output [1,6], [8,10]. (Tests sorting and custom collection logic).
+        */
+
+        int[][] intervals = {{1,3}, {2,6}, {8,10}, {15,18}, {9,11}};
+
+
+
+
 
     }
 
