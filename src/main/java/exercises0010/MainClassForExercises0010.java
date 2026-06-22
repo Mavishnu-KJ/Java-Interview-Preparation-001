836486e39c9eb4fbb99bc1d6e1ba30afd840d777
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-06-22 09:52:20 +0530

    Java 8-17 Coding practices

---
+++ src/main/java/exercises0010/MainClassForExercises0010.java
@@ -0,0 +1,11 @@
+package exercises0010;
+
+public class MainClassForExercises0010 {
+
+    public static void main(String[] args){
+
+        //System.out.println("TEST MainClassForExercises0010");
+
+    }
+
+}
