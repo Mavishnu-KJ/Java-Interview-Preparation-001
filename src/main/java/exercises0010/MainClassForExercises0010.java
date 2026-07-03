bdc4188ffc59e9a07c0bd62f6aedda7bb07963fc
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-07-03 14:45:12 +0530

    Java 8-17 Coding practices

--- src/main/java/exercises0010/MainClassForExercises0010.java
+++ src/main/java/exercises0010/MainClassForExercises0010.java
@@ -1,10 +1,46 @@
 package exercises0010;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class MainClassForExercises0010 {
 
     public static void main(String[] args){
 
         //System.out.println("TEST MainClassForExercises0010");
+
+        //Welcome To Programming
+/*
+        String inputString = "Welcome To Programming";
+
+        Map<Character, Long> freqMap = inputString.chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> Character.isLetterOrDigit(c))
+                .collect(Collectors.groupingBy(
+                        c -> c,
+                        LinkedHashMap:: new,
+                        Collectors.counting()
+                )).entrySet().stream()
+                .sorted(Comparator.comparingLong(Map.Entry :: getValue))
+                        .collect(Collectors.groupingBy(
+                                        Map.Entry :: getKey,
+                                        Map.Entry :: getValue
+
+                                ));
+
+
+
+
+        freqMap.forEach((character, freq) -> {
+          System.out.println(character+" repeated "+freq+" times");
+        });
+
+*/
+
+
 
     }
 
---
+++ src/main/java/exercises0011/MainClassForExercises0011.java
@@ -0,0 +1,12 @@
+package exercises0011;
+
+public class MainClassForExercises0011 {
+
+    public static void main(String[] args){
+
+        System.out.println("TEST MainClassForExercises0011");
+
+
+    }
+
+}
